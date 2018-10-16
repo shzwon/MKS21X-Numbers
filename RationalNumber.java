8c80b2c44f98bd7fed1a670623ed792dc1f3b326
shzwon <rwu5@stuy.edu>
2018-10-16 11:41:04 -0400

    pushing

---
+++ RationalNumber.java
@@ -0,0 +1,18 @@
+public class RationalNumber extends RealNumber {
+  private double x;
+  public RationalNumber(){
+    x = 0;
+  }
+  public RationalNumber(double num){
+    x = 0;
+  }
+  public int getX(){
+    return 0;
+  }
+  public String toString(){
+    return "" + x;
+  }
+  public double compareTo(double y, double z){
+    return 0;
+  }
+}
---
+++ RealNumber.java
@@ -0,0 +1,16 @@
+public class RealNumber{
+  public RealNumber() {
+
+}
+
+  public RealNumber(int x) {
+
+}
+
+  public String toString(){
+    return "" ;
+  }
+  public double compareTo(double y, double z){
+    return 0;
+  }
+}
