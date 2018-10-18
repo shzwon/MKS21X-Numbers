04c8c1368c8a6f6d21a3ad8b831186d0b7310f87
shzwon <rwu5@stuy.edu>
2018-10-18 10:44:19 -0400

    sending

--- RealNumber.java
+++ RealNumber.java
@@ -1,15 +1,34 @@
 public class RealNumber{
-  public RealNumber() {
+  private double value;
 
+  public RealNumber(double x) {
+    value = x;
 }
 
-  public RealNumber(int x) {
-
+  public double getValue() {
+	  return value;
 }
 
   public String toString(){
-    return "" ;
+    return "" + value;
   }
+
+  public double add(RealNumber other){
+    return value + other.getValue();
+  }
+
+  public double subtract(RealNumber other){
+   return value - other.getValue();
+ }
+
+ public double multiply(RealNumber other){
+   return value * other.getValue();
+ }
+
+ public double divide(RealNumber other){
+    return value/other.getValue();
+  }
+
   public double compareTo(double y, double z){
     return 0;
   }
